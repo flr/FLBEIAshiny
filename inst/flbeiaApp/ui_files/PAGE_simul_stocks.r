da31e3fb46d60a260a7efc8380bb0aafd23cba74
Maria <mkorta@azti.es>
2019-10-21 13:25:18 +0200

    add collapsed panels
    otherwise sidebar too long

--- inst/flbeiaApp/ui_files/PAGE_simul_stocks.r
+++ inst/flbeiaApp/ui_files/PAGE_simul_stocks.r
@@ -7,27 +7,34 @@ tabsetPanel(type = "tabs",
             #------------------------------------
             tabPanel( 
               title = "Time series",
-                sidebarLayout(
-                sidebarPanel(
+              fluidRow(
+                br(),
+                column(3,
+                      bsCollapse(id = "collapse", #open = "Stock and Indicator",
+                      bsCollapsePanel("Stock and Indicator",
                   sliderInput("rangeS", label=h4("Years"), min(bio$year), max(bio$year), value=range(bio$year),step = 1),
                   selectizeInput("stockS", label=h4("Stock"), levels(as.factor(bio$stock)), selected=unique(bio$stock)[1],multiple=T, options=list(plugins=list("remove_button", "drag_drop"))),
                   selectizeInput("indicatorS", label=h4("Indicators"), levels(as.factor(bio$indicator)),selected=unique(bio$indicator)[1],multiple=T, options=list(plugins=list("remove_button", "drag_drop"))),
-                  selectizeInput("scenarioS", label=h4("Scenarios"), levels(as.factor(bio$scenario)), selected=unique(bio$scenario)[1], multiple=T, options=list(plugins=list("remove_button", "drag_drop"))),
-                  hr(),
+                  selectizeInput("scenarioS", label=h4("Scenarios"), levels(as.factor(bio$scenario)), selected=unique(bio$scenario)[1], multiple=T, options=list(plugins=list("remove_button", "drag_drop")))#,
+                  #hr(),
+                      ),
+                  bsCollapsePanel("Graphs",
                   checkboxInput("fitCIS", h5("Confident interval"), FALSE),
-                  checkboxInput("fitS", h5("Free scales"), FALSE),
-                  
+                  checkboxInput("fitS", h5("Free scales"), FALSE)
+                  ),
+                  bsCollapsePanel("Download",
                   # Options for file downloading
                   textInput('filenmS', h5("File Name"), value = "", width = NULL, placeholder = NULL),
                   numericInput('fileWS', h5("Width (cm)"), value = 14, min = 0, max = 25, step = 1, width = 75),
                   numericInput('fileHS', h5("Height (cm)"), value = 10, min = 0, max = 25, step = 1, width = 75),
                   numericInput('fileScS', h5("Scale in ggsave"), value = 1.5, min = 0, max = 3, step = 0.1, width = 75),
                   selectInput(inputId = "fileTypeS", label = "Select the file type", selected= "png", choices = c("eps", "ps", "pdf", "jpeg", "tiff", "png", "bmp", "svg", "wmf"), multiple = FALSE),
-                  downloadButton(outputId = "down", label = "Download the plot")),
+                  downloadButton(outputId = "down", label = "Download the plot")
+                  ))),
                 
-            mainPanel(
-                  plotOutput("plotS"))
-            )),
+                column(9,
+                  plotOutput("plotS", height = "600px", width = "900px")
+                ))),
             
             
             #------------------------------------
@@ -83,3 +90,4 @@ tabsetPanel(type = "tabs",
                 )
             ))
 )#end of tabsetPanel
+

a9c38fc6bdfd717c6b3fe4d846b64159890e00a1
Maria <mkorta@azti.es>
2019-10-22 14:24:48 +0200

    include bscollapsePanel biological risk

--- inst/flbeiaApp/ui_files/PAGE_simul_stocks.r
+++ inst/flbeiaApp/ui_files/PAGE_simul_stocks.r
@@ -47,7 +47,7 @@ tabsetPanel(type = "tabs",
                 br(),
                 column(3,
                        bsCollapse(id = "collapse", #open = "Stock and Indicator",
-                       bsCollapsePanel("Stock and Indicator",
+                       bsCollapsePanel("Stock and scenarios",
                   sliderInput("rangeK", label=h4("Years"), min(bio$year), max(bio$year), value=range(bio$year),step = 1),
                   selectizeInput("stockK", label=h4("Stock"),        unique(RefPts$stock),  selected=unique(RefPts$stock)[1],       multiple=T, options=list(plugins=list("remove_button", "drag_drop"))),
                   selectizeInput("scenarioK", label=h4("Scenarios"), unique(as.factor(bio$scenario)), selected=unique(RefPts$scenario)[1], multiple=T, options=list(plugins=list("remove_button", "drag_drop")))
@@ -73,25 +73,28 @@ tabsetPanel(type = "tabs",
             #------------------------------------
             tabPanel(
               title = "Biological risk", 
-              sidebarLayout(
-                sidebarPanel(
+              fluidRow(
+                br(),
+                column(3,
+                       bsCollapse(id = "collapse", #open = "Stock and Indicator",
+                       bsCollapsePanel("Stock and reference points",
                   sliderInput("rangeR", label=h4("Years"), min(as.numeric(risk$year)), max(as.numeric(risk$year)), value=range(as.numeric(risk$year)),step = 1),
                   selectizeInput("stockR", label=h4("Stock"), choices= unique((RefPts$stock)), selected=unique((RefPts$stock))[1],multiple=T, options=list(plugins=list("remove_button", "drag_drop"))),
                   selectizeInput("scenarioR", label=h4("Scenarios"), levels(as.factor(risk$scenario)), selected= unique((risk$scenario))[1], multiple=T, options=list(plugins=list("remove_button", "drag_drop"))),
-                  selectizeInput("brpR", label=h4("Biological reference point"), choices=c("pBlim", "pBpa"),selected="pBlim", multiple=T, options=list(plugins=list("remove_button", "drag_drop"))),
-                  
+                  selectizeInput("brpR", label=h4("Biological reference point"), choices=c("pBlim", "pBpa"),selected="pBlim", multiple=T, options=list(plugins=list("remove_button", "drag_drop")))
+                       ),
+                  bsCollapsePanel("Download",
                   # Options for file downloading
                   textInput('filenmSR', h5("File Name"), value = "", width = NULL, placeholder = NULL),
                   numericInput('fileWSR', h5("Width (cm)"), value = 14, min = 0, max = 25, step = 1, width = 75),
                   numericInput('fileHSR', h5("Height (cm)"), value = 10, min = 0, max = 25, step = 1, width = 75),
                   numericInput('fileScSR', h5("Scale in ggsave"), value = 1.5, min = 0, max = 3, step = 0.1, width = 75),
                   selectInput(inputId = "fileTypeSR", label = "Select the file type", selected= "png", choices = c("eps", "ps", "pdf", "jpeg", "tiff", "png", "bmp", "svg", "wmf"), multiple = FALSE),
-                  downloadButton(outputId = "downSR", label = "Download the plot"),
-                  hr()
-                ),
-                mainPanel(
-                  plotOutput("plotR")
-                )
-            ))
+                  downloadButton(outputId = "downSR", label = "Download the plot")
+                  ))),
+                
+                column(9,
+                  plotOutput("plotR", height = "600px", width = "900px")
+                )))
 )#end of tabsetPanel
 

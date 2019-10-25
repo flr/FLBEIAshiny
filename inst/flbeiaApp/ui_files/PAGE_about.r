4e46f69a62eb80468614c21dfdf784189368ab72
Maria <mkorta@azti.es>
2019-10-25 14:33:18 +0200

    inputext

--- inst/flbeiaApp/ui_files/PAGE_about.r
+++ inst/flbeiaApp/ui_files/PAGE_about.r
@@ -1,5 +1,10 @@
 tabPanel("FLBEIA",
            br(),
-           includeHTML ("data/DescriptionFlbeia.txt")
+           includeHTML ("data/DescriptionFlbeia.txt"), 
+           textAreaInput("caption", "", rows = 5, "Describe your case study here...", width = "1000px"),
+           textOutput("value"),
+           br(),
+           br(),
+           actionButton("submit", "Add")
            )
 

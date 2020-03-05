c549e70f9ad0131df461f73431f39a0b3d9079da
Maria <mkorta@azti.es>
2020-03-05 17:16:01 +0100

    CS upload modified

--- inst/flbeiaApp/ui_files/PAGE_about.r
+++ inst/flbeiaApp/ui_files/PAGE_about.r
@@ -1,10 +1,12 @@
 tabPanel("FLBEIA",
            br(),
            includeHTML ("data/DescriptionFlbeia.txt"), 
-           textAreaInput("caption", "", rows = 5, "Describe your case study here...", width = "1000px"),
-           textOutput("value"),
+           #includeHTML ("data/DescriptionCS.txt")#,  
+           #textAreaInput("caption", "", rows = 5, "Describe your case study here...", width = "1000px"),
            br(),
-           br(),
-           actionButton("submit", "Add")
+           tableOutput("cs")#,
+           # br(),
+           # br(),
+           # actionButton("submit", "Add")
            )
 
